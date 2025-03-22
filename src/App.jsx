a26b0887d80da4de5cc280a3b7b929975211bb1c
siddhesh <siddheshneharkar70@gmail.com>
2025-03-22 16:36:47 +0530

    Add responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Header from './components/Header/Header.jsx';
+import Hero from './components/Hero/Hero.jsx';
+import './App.css';
+import Companies from './components/Companies/Companies.jsx';
+import Residencies from './components/Residencies/Residencies.jsx';
+import Value from './components/Value/Value.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import GetStarted from './components/GetStarted/GetStarted.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
+    </div>
   );
 }
 
